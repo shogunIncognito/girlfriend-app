4509375323af7e668d370cacf2f701395917ec8b
BeiGod <saidsamuel2006@hotmail.com>
2022-10-17 13:32:40 -0500

    change img src

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ bYes.addEventListener('click', () => {
         <div class="yRes">
             <h1>💖Eso pensé💖</h1>
             <div class="images">
-                <img width="300px" src="../img/corazones.webp" alt="Amor">
-                <img width="200px" src="../img/osito.webp" alt="Amor">
-                <img width="300px" src="../img/amor.webp" alt="Amor">
+                <img width="300px" src="./img/corazones.webp" alt="Amor">
+                <img width="200px" src="./img/osito.webp" alt="Amor">
+                <img width="300px" src="./img/amor.webp" alt="Amor">
             </div>
         </div>
         <button id="regre">Regresar</button>
